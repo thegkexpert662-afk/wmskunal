ffc2056ed36f2f126e6183db2fe117ff4128b348
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-17 18:35:58 +0530

    Add Stock Excel MIS portal screen

---
+++ lib/screens/stock_mis_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StockMisScreen extends StatelessWidget {
+  const StockMisScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Stock / Excel MIS', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900, color: Color(0xFF10243E))));
+  }
+}
