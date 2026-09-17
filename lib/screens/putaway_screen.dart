d10bad30c5ea9caa2a04a8484c9ac00a5476a895
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-17 18:35:51 +0530

    Add PUT portal screen

---
+++ lib/screens/putaway_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PutawayScreen extends StatelessWidget {
+  const PutawayScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('PUT', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900, color: Color(0xFF10243E))));
+  }
+}
