c8740fee4cc56d3cde87ba7a0c3756aeefd93a1d
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:34:07 +0530

    Organize admin role screen

---
+++ lib/screens/admin/invoice_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../invoice_screen.dart';
+
+class AdminInvoiceScreen extends StatelessWidget {
+  const AdminInvoiceScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const InvoiceScreen();
+}
