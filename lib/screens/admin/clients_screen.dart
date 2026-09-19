2812e03e0eebd947c05b101be93025976e3c4136
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:38:07 +0530

    Fix admin screen common widgets import

--- lib/screens/admin/clients_screen.dart
+++ lib/screens/admin/clients_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'common_widgets.dart';
+import '../../common_widgets.dart';
 
 class AdminClientsScreen extends StatefulWidget {
   const AdminClientsScreen({super.key});
