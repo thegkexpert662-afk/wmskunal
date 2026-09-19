d40a1657e163d2df13da6ff52a6178d7c44ac68c
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:48:39 +0530

    Create unique Place Order client screen

--- lib/screens/client/place_order_screen.dart
+++ lib/screens/client/place_order_screen.dart
@@ -1,3 +1,41 @@
 import 'package:flutter/material.dart';
-import '../client_portal_screen.dart';
-class ClientPlaceOrderScreen extends StatelessWidget { const ClientPlaceOrderScreen({super.key}); @override Widget build(BuildContext context) => const ClientPortalScreen(); }
+import '../../common_widgets.dart';
+
+class ClientPlaceOrderScreen extends StatelessWidget {
+  const ClientPlaceOrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenFrame(
+      title: 'Place Order',
+      subtitle: 'Create a new client order from authorised products.',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Search product to add to order',
+              prefixIcon: const Icon(Icons.add_shopping_cart_outlined),
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(28),
+              child: Row(
+                children: [
+                  Icon(Icons.add_shopping_cart_outlined, size: 42),
+                  const SizedBox(width: 16),
+                  const Expanded(
+                    child: Text('This client screen is isolated from other client accounts. Data will be loaded from the secured WMS API.'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
