b7efe619026410f4d130cc6c8d0e61e393759021
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:34:01 +0530

    Organize admin role screen

---
+++ lib/screens/admin/packing_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../packing_screen.dart';
+
+class AdminPackingScreen extends StatelessWidget {
+  const AdminPackingScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const PackingScreen();
+}
