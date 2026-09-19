2bd7de7f3907c2e6d8da17240279877f3cd6fd1c
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:20 +0530

    Organize master_admin role screen

---
+++ lib/screens/master_admin/companies_admin_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../master_admin_screen.dart';
+
+class CompaniesAdminScreen extends StatelessWidget {
+  const CompaniesAdminScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const MasterAdminScreen();
+}
