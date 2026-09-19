dbc7c545827595f3dccd3649faec3f3075a672cb
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:44:09 +0530

    Fix common widgets import path

--- lib/screens/client/track_dispatch_screen.dart
+++ lib/screens/client/track_dispatch_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../common_widgets.dart';
+import '../common_widgets.dart';
 
 class ClientTrackDispatchScreen extends StatelessWidget {
   const ClientTrackDispatchScreen({super.key});
