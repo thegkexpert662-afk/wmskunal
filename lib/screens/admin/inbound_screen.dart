902279ef4ebd4145e57522d02602682c8d643f01
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:42:48 +0530

    Remove duplicate wrapper lib/screens/admin/inbound_screen.dart

--- lib/screens/admin/inbound_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../inbound_screen.dart';
-
-class AdminInboundScreen extends StatelessWidget {
-  const AdminInboundScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const InboundScreen();
-}
