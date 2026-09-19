61e5078c8829130c1de37b231e0a7abb5df89af9
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:46:26 +0530

    Remove duplicate admin dashboard wrapper

--- lib/screens/admin/dashboard_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../dashboard_screen.dart';
-
-class AdminDashboardScreen extends StatelessWidget {
-  const AdminDashboardScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const DashboardScreen();
-}
