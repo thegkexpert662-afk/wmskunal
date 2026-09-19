ee752103c1d5620c7e4a6a56ae5579a6a7c346b4
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:52:50 +0530

    Fix common widgets import path

--- lib/screens/master_admin/system_settings_screen.dart
+++ lib/screens/master_admin/system_settings_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'common_widgets.dart';
+import '../common_widgets.dart';
 
 class MasterSystemSettingsScreen extends StatelessWidget {
   const MasterSystemSettingsScreen({super.key});
