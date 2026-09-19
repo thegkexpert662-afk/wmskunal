c9a34f5f78cc66e0329b7c664d91e756e157cfce
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:46:41 +0530

    Fix dispatch screen common widgets import

--- lib/screens/admin/dispatch_screen.dart
+++ lib/screens/admin/dispatch_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'common_widgets.dart';
+import '../common_widgets.dart';
 
 class AdminDispatchScreen extends StatefulWidget {
   const AdminDispatchScreen({super.key});
