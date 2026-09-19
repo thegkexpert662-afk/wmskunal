41efd66d143505f50c6083080c900efcc23d1870
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:34:10 +0530

    Organize admin role screen

---
+++ lib/screens/admin/stock_mis_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../stock_mis_screen.dart';
+
+class AdminStockMisScreen extends StatelessWidget {
+  const AdminStockMisScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const StockMisScreen();
+}
