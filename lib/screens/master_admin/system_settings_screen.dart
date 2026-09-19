d1d8d1810f384c14a7a7551cce4f15cf9ea153ec
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:28 +0530

    Organize master_admin role screen

---
+++ lib/screens/master_admin/system_settings_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../settings_screen.dart';
+
+class MasterSystemSettingsScreen extends StatelessWidget {
+  const MasterSystemSettingsScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const SettingsScreen();
+}
