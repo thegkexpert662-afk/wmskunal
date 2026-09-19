ee19c2c9233279a34da46f33d3c5b3a34bd80e08
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:43 +0530

    Organize admin role screen

---
+++ lib/screens/admin/inbound_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../inbound_screen.dart';
+
+class AdminInboundScreen extends StatelessWidget {
+  const AdminInboundScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const InboundScreen();
+}
