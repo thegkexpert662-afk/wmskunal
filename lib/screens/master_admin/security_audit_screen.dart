08ad5f9251a8ccabcfeff09f30cb838e804e86a8
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:31 +0530

    Organize master_admin role screen

---
+++ lib/screens/master_admin/security_audit_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../audit_logs_screen.dart';
+
+class MasterSecurityAuditScreen extends StatelessWidget {
+  const MasterSecurityAuditScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const AuditLogsScreen();
+}
