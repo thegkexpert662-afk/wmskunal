21d38e0dfd6177a67408268010667ef27269df7e
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:46:10 +0530

    Remove duplicate wrapper lib/screens/admin/reports_screen.dart

--- lib/screens/admin/reports_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../reports_screen.dart';
-
-class AdminReportsScreen extends StatelessWidget {
-  const AdminReportsScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const ReportsScreen();
-}
