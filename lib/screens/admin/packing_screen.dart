c2891ed99bd2be0b370c5a66d432e4e24f1aae93
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:44:59 +0530

    Remove duplicate wrapper lib/screens/admin/packing_screen.dart

--- lib/screens/admin/packing_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../packing_screen.dart';
-
-class AdminPackingScreen extends StatelessWidget {
-  const AdminPackingScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const PackingScreen();
-}
