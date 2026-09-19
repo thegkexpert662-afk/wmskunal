59a21aa1b24d878fdb58c1c510d1ab75bda597b7
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:38 +0530

    Organize admin role screen

---
+++ lib/screens/admin/dashboard_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../dashboard_screen.dart';
+
+class AdminDashboardScreen extends StatelessWidget {
+  const AdminDashboardScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const DashboardScreen();
+}
