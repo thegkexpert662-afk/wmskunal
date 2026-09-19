620f2e391af21d508f380d0ad46b35cbc990d1f4
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:54 +0530

    Organize admin role screen

---
+++ lib/screens/admin/orders_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../orders_screen.dart';
+
+class AdminOrdersScreen extends StatelessWidget {
+  const AdminOrdersScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const OrdersScreen();
+}
