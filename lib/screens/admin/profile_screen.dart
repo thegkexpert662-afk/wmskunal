a2df4e54a0f721826ce35eef99cd89babdf69125
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:46:00 +0530

    Remove duplicate wrapper lib/screens/admin/profile_screen.dart

--- lib/screens/admin/profile_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../profile_screen.dart';
-
-class AdminProfileScreen extends StatelessWidget {
-  const AdminProfileScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const ProfileScreen();
-}
