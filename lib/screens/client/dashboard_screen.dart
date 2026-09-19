08f85b0a4984bc2d90eae3b38371aaa5e5c3d47d
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:34:22 +0530

    Organize client role screen

---
+++ lib/screens/client/dashboard_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../client_portal_screen.dart';
+
+class ClientDashboardScreen extends StatelessWidget {
+  const ClientDashboardScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const ClientPortalScreen();
+}
