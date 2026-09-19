4fadb4171b2560c3c3ba7124d34af01624e863cc
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:52:28 +0530

    Fix common widgets import path

--- lib/screens/admin/reports_screen.dart
+++ lib/screens/admin/reports_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'common_widgets.dart';
+import '../common_widgets.dart';
 
 class AdminReportsScreen extends StatelessWidget {
   const AdminReportsScreen({super.key});
