831a252622b206e2f9ef694d4e4935adafdbcce0
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:44:34 +0530

    Remove duplicate wrapper lib/screens/admin/orders_screen.dart

--- lib/screens/admin/orders_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../orders_screen.dart';
-
-class AdminOrdersScreen extends StatelessWidget {
-  const AdminOrdersScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const OrdersScreen();
-}
