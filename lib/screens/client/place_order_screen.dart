5806ac92f42a89a8ff4c793d0d8160b189608a68
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:37:14 +0530

    Add role screen lib/screens/client/place_order_screen.dart

---
+++ lib/screens/client/place_order_screen.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import '../client_portal_screen.dart';
+class ClientPlaceOrderScreen extends StatelessWidget { const ClientPlaceOrderScreen({super.key}); @override Widget build(BuildContext context) => const ClientPortalScreen(); }
