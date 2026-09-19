20f5d8fab9b2ed399c470ffd3eef487c664303a8
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:45 +0530

    Organize admin role screen

---
+++ lib/screens/admin/putaway_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../putaway_screen.dart';
+
+class AdminPutawayScreen extends StatelessWidget {
+  const AdminPutawayScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const PutawayScreen();
+}
