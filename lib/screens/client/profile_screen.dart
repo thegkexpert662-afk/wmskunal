42299faf306b3e7308d237d8795189081fb75410
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:37:41 +0530

    Add client screen lib/screens/client/profile_screen.dart

---
+++ lib/screens/client/profile_screen.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import '../profile_screen.dart';
+class ClientProfileScreen extends StatelessWidget { const ClientProfileScreen({super.key}); @override Widget build(BuildContext context) => const ProfileScreen(roleLabel: 'Client'); }
