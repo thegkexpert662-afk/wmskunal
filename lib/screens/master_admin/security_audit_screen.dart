1e9f3df1df1c83981cd71aa6e24f9bc14218e5df
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:47:41 +0530

    Remove duplicate master wrapper lib/screens/master_admin/security_audit_screen.dart

--- lib/screens/master_admin/security_audit_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../audit_logs_screen.dart';
-
-class MasterSecurityAuditScreen extends StatelessWidget {
-  const MasterSecurityAuditScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const AuditLogsScreen();
-}
