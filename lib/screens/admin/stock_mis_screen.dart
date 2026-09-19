1adca1cc75e10a11be6fbc1c9377ebdeeb349e99
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:45:51 +0530

    Move stock_mis_screen.dart into admin folder

---
+++ lib/screens/admin/stock_mis_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminStockMisScreen extends StatelessWidget {
+  const AdminStockMisScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Stock / Excel MIS', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900, color: Color(0xFF10243E))));
+  }
+}
