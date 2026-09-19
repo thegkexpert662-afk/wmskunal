b5c21cb5644973b96b141ccccfacb1fd8122f9ce
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:48:58 +0530

    Create unique Profile client screen

--- lib/screens/client/profile_screen.dart
+++ lib/screens/client/profile_screen.dart
@@ -1,3 +1,41 @@
 import 'package:flutter/material.dart';
-import '../profile_screen.dart';
-class ClientProfileScreen extends StatelessWidget { const ClientProfileScreen({super.key}); @override Widget build(BuildContext context) => const ProfileScreen(roleLabel: 'Client'); }
+import '../../common_widgets.dart';
+
+class ClientProfileScreen extends StatelessWidget {
+  const ClientProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenFrame(
+      title: 'Profile',
+      subtitle: 'View the logged-in client profile and account information.',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Account information',
+              prefixIcon: const Icon(Icons.person_outline),
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(28),
+              child: Row(
+                children: [
+                  Icon(Icons.person_outline, size: 42),
+                  const SizedBox(width: 16),
+                  const Expanded(
+                    child: Text('This client screen is isolated from other client accounts. Data will be loaded from the secured WMS API.'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
