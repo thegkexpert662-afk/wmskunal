bb3bd6d793c38081874e0ca9278d1d17a4838099
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:48 +0530

    Organize admin role screen

---
+++ lib/screens/admin/clients_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../clients_screen.dart';
+
+class AdminClientsScreen extends StatelessWidget {
+  const AdminClientsScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const ClientsScreen();
+}
