5e90342cbb66bbe8fd6e047877a265dc9cc6b5b8
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:52:43 +0530

    Fix common widgets import path

--- lib/screens/master_admin/security_audit_screen.dart
+++ lib/screens/master_admin/security_audit_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'common_widgets.dart';
+import '../common_widgets.dart';
 
 class MasterSecurityAuditScreen extends StatefulWidget {
   const MasterSecurityAuditScreen({super.key});
