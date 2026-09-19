38a81cabbd66066a31bcdeb0f7c3819836462912
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:37:09 +0530

    Add role screen lib/screens/client/product_catalogue_screen.dart

---
+++ lib/screens/client/product_catalogue_screen.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import '../client_portal_screen.dart';
+class ClientProductCatalogueScreen extends StatelessWidget { const ClientProductCatalogueScreen({super.key}); @override Widget build(BuildContext context) => const ClientPortalScreen(); }
