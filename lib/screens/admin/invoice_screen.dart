5294a8243f4cc48d8863adaea7c6168b85bbac49
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:45:32 +0530

    Remove duplicate wrapper lib/screens/admin/invoice_screen.dart

--- lib/screens/admin/invoice_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../invoice_screen.dart';
-
-class AdminInvoiceScreen extends StatelessWidget {
-  const AdminInvoiceScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const InvoiceScreen();
-}
