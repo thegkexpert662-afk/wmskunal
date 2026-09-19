7d6e08d11bd35a557d17311321610b86ea4927a2
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:46:56 +0530

    Remove duplicate master wrapper lib/screens/master_admin/master_dashboard_screen.dart

--- lib/screens/master_admin/master_dashboard_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../master_admin_screen.dart';
-
-class MasterDashboardScreen extends StatelessWidget {
-  const MasterDashboardScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const MasterAdminScreen();
-}
