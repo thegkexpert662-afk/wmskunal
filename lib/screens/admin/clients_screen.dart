2f1528770fb0ef6ffb3d89dc533cca2ff95538b4
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:43:15 +0530

    Remove duplicate wrapper lib/screens/admin/clients_screen.dart

--- lib/screens/admin/clients_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../clients_screen.dart';
-
-class AdminClientsScreen extends StatelessWidget {
-  const AdminClientsScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const ClientsScreen();
-}
