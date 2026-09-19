fd8da070b8371af9fb4015b3082d4d6f92a5eece
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:43:00 +0530

    Remove duplicate wrapper lib/screens/admin/putaway_screen.dart

--- lib/screens/admin/putaway_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../putaway_screen.dart';
-
-class AdminPutawayScreen extends StatelessWidget {
-  const AdminPutawayScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const PutawayScreen();
-}
