ae942772dabd20cc9c159217024d90eb9956ae6b
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:52:58 +0530

    Fix common widgets import path

--- lib/screens/master_admin/users_roles_screen.dart
+++ lib/screens/master_admin/users_roles_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'common_widgets.dart';
+import '../common_widgets.dart';
 
 class MasterUsersRolesScreen extends StatefulWidget {
   const MasterUsersRolesScreen({super.key});
