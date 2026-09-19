1b11e9f15cd5a032fa68dba999b1ae450bdcb95c
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:37:29 +0530

    Add client screen lib/screens/client/track_dispatch_screen.dart

---
+++ lib/screens/client/track_dispatch_screen.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import '../client_portal_screen.dart';
+class ClientTrackDispatchScreen extends StatelessWidget { const ClientTrackDispatchScreen({super.key}); @override Widget build(BuildContext context) => const ClientPortalScreen(); }
