9266f559af2d1c7a7dd38ed767e49f46932574c0
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 13:45:12 +0530

    Remove duplicate wrapper lib/screens/admin/dispatch_screen.dart

--- lib/screens/admin/dispatch_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../dispatch_screen.dart';
-
-class AdminDispatchScreen extends StatelessWidget {
-  const AdminDispatchScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const DispatchScreen();
-}
