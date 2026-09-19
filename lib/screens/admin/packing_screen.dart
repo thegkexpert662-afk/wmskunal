91392cffdba7d0eec7cc089de5536e03b5ce6438
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 18:52:07 +0530

    Fix common widgets import path

--- lib/screens/admin/packing_screen.dart
+++ lib/screens/admin/packing_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'common_widgets.dart';
+import '../common_widgets.dart';
 
 class AdminPackingScreen extends StatefulWidget {
   const AdminPackingScreen({super.key});
