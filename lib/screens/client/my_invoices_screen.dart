23eecf09094b4d78dbff7d9b1624e9a71086fc7b
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:37:33 +0530

    Add client screen lib/screens/client/my_invoices_screen.dart

---
+++ lib/screens/client/my_invoices_screen.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import '../client_portal_screen.dart';
+class ClientMyInvoicesScreen extends StatelessWidget { const ClientMyInvoicesScreen({super.key}); @override Widget build(BuildContext context) => const ClientPortalScreen(); }
