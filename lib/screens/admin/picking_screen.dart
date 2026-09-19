5cc855107c35f8ec6ca393bf4818028f3f2bf241
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-19 12:33:57 +0530

    Organize admin role screen

---
+++ lib/screens/admin/picking_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../picking_screen.dart';
+
+class AdminPickingScreen extends StatelessWidget {
+  const AdminPickingScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const PickingScreen();
+}
