09119008d1133ccb40c324e4a2007909593231cb
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-21 03:58:20 +0530

    Add bottom horizontal table scrollbar

--- lib/screens/admin/packing_screen.dart
+++ lib/screens/admin/packing_screen.dart
@@ -236,8 +236,7 @@ class _AdminPackingScreenState extends State<AdminPackingScreen> {
             ),
           ),
           const Divider(height: 1),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
+          HorizontalTableScroller(
             child: DataTable(
               headingTextStyle: const TextStyle(
                 color: Color(0xFF43546A),
