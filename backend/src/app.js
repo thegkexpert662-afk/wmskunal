a0ea7b9831742d05b47c31a1d90a3edfbd86511f
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-23 12:34:03 +0530

    feat: mount user management API

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ const grnRoutes = require('./routes/grn.routes');
 const deviceRoutes = require('./routes/device.routes');
 const companyRoutes = require('./routes/company.routes');
 const clientRoutes = require('./routes/client.routes');
+const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
@@ -47,6 +48,7 @@ app.use('/api/grns', grnRoutes);
 app.use('/api/devices', deviceRoutes);
 app.use('/api/company', companyRoutes);
 app.use('/api/clients', clientRoutes);
+app.use('/api/users', userRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
