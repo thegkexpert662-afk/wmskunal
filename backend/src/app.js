c02c87f240e80929c23e88dff9afed3ec9e7475f
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-23 17:06:50 +0530

    feat(inventory): register inventory API

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,7 @@ const clientRoutes = require('./routes/client.routes');
 const userRoutes = require('./routes/user.routes');
 const warehouseRoutes = require('./routes/warehouse.routes');
 const productRoutes = require('./routes/product.routes');
+const inventoryRoutes = require('./routes/inventory.routes');
 
 const app = express();
 
@@ -59,6 +60,7 @@ app.use('/api/clients', clientRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/warehouses', warehouseRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/inventory', inventoryRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
