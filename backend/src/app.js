80d7252041d86286fc5d591388ab3bbcd89e26ec
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-23 12:53:51 +0530

    Register product master API routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const companyRoutes = require('./routes/company.routes');
 const clientRoutes = require('./routes/client.routes');
 const userRoutes = require('./routes/user.routes');
 const warehouseRoutes = require('./routes/warehouse.routes');
+const productRoutes = require('./routes/product.routes');
 
 const app = express();
 
@@ -51,6 +52,7 @@ app.use('/api/company', companyRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/warehouses', warehouseRoutes);
+app.use('/api/products', productRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
